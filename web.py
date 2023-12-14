cf496ff567f319dbec6a89f993829add8488eb1d
zirangu <ziran.gu0224@gmail.com>
2023-12-14 09:47:25 -0500

    Update web.py
    Add functionality of viewing country temperature trend

--- web.py
+++ web.py
@@ -20,6 +20,13 @@ def load_data():
 
     return df_grouped
 
+
+# Create the line plot for a selected country
+def create_line_plot(df, country):
+    df_country = df[df['Country'] == country]
+    fig = px.line(df_country, x='year', y='AverageTemperature', title=f'Temperature Trend for {country}')
+    return fig
+
 # Create the Streamlit app
 def main():
     st.title("Geographic Heatmap of Temperature Changes Over Time")
@@ -45,7 +52,14 @@ def main():
 
     st.plotly_chart(fig, use_container_width=True)  # Set to use the full width of the Streamlit container
 
+    # Country selection for line plot
+    countries = df['Country'].unique()
+    selected_country = st.selectbox("Select a Country", countries)
 
+    # Display line plot for the selected country
+    if selected_country:
+        line_fig = create_line_plot(df, selected_country)
+        st.plotly_chart(line_fig)
 
 if __name__ == "__main__":
     main()
